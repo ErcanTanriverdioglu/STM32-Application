839080f94ed2c7f8ff75805ffe772f56797d48b1
Ercan Tanrıverdioğlu <122203202+ErcanTanriverdioglu@users.noreply.github.com>
2023-07-19 15:32:34 +0300

    main.c

--- PARK_SENSOR/Core/Src/main.c
+++ PARK_SENSOR/Core/Src/main.c
@@ -118,7 +118,7 @@ int main(void)
   MX_TIM1_Init();
   /* USER CODE BEGIN 2 */
 
-  	HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1); 	//7199 PERİOD DEĞERİ
+    HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1); 	
     DWT_Init();
     SSD1306_Init();
     SSD1306_Clear();
